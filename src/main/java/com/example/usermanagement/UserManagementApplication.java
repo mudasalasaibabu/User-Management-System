3a5290e96ff35abfd68d700b435ff4790a3331b3
SaiBabu <mudasalasaibabu@gmail.com>
2025-12-31 12:48:45 +0530

    Fix WAR startup, optimize build, and skip tests for deployment

--- src/main/java/com/example/usermanagement/UserManagementApplication.java
+++ src/main/java/com/example/usermanagement/UserManagementApplication.java
@@ -2,12 +2,18 @@ package com.example.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserManagementApplication {
+public class UserManagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementApplication.class, args);
-	}	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserManagementApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementApplication.class, args);
+    }
 }
