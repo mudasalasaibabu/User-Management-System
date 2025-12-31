39756fc5095df9f3da589d7879b554aea80914fe
SaiBabu <mudasalasaibabu@gmail.com>
2025-12-31 15:41:51 +0530

    Reduce startup to ~60s: lazy JPA repos + tomcat tuning

--- src/main/java/com/example/usermanagement/UserManagementApplication.java
+++ src/main/java/com/example/usermanagement/UserManagementApplication.java
@@ -2,12 +2,11 @@ package com.example.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 
-@SpringBootApplication(
-		  exclude = {
-		    org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class
-		  }
-		)
+@SpringBootApplication
+@EnableJpaRepositories(bootstrapMode = BootstrapMode.LAZY)
 public class UserManagementApplication {
 
     public static void main(String[] args) {
