85e756962f010e7e7fa1ce3107fbd3d431e56f24
SaiBabu <mudasalasaibabu@gmail.com>
2026-01-27 17:42:42 +0530

    Fix LazyInitializationException using @Transactional

--- src/main/java/com/example/usermanagement/serviceimplementation/CourseAnalyticsServiceImp.java
+++ src/main/java/com/example/usermanagement/serviceimplementation/CourseAnalyticsServiceImp.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.usermanagement.dto.CourseAnalyticsDTO;
 import com.example.usermanagement.dto.EnrolledUserDTO;
@@ -20,6 +21,7 @@ public class CourseAnalyticsServiceImp implements CourseAnalyticsService {
 	private UserCourseRepository userCourseRepository;
 	@Autowired
 	private CourseRepository courseRepository;
+	@Transactional(readOnly = true)
 	@Override
 	public CourseAnalyticsDTO getCourseAnalytics(Long courseId) {
 		 Course course = courseRepository.findById(courseId)
